19f89ff59071a8b06a723f1c9df71b2a87fafdf3
Scott Silverman <scottms724@gmail.com>
2019-07-10 17:05:52 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -10,6 +10,8 @@ class Song < ActiveRecord::Base
   end
 
    def self.find_by_slug(slug)
+     @name = slug.split("-").map!{|word| word.capitalize}.join(" ")
+     @name = slug.gsub(' ', '-').upcase
     special = ["with", "the", "a", "or", "and"]
     array = []
     slug.split("-").each do |word|
